bd2321a7a0e68e8f8e4996e4a7c61eb33847c772
danyllorobemar <danyllo.robemar@gmail.com>
2024-09-10 19:53:28 -0300

    Inital commit

---
+++ src/br/com/devdan/App.java
@@ -0,0 +1,11 @@
+package br.com.devdan;
+
+public class App {
+    public static void main(String[] args) {
+        int n1 = 10;
+        System.out.println(n1);
+
+        Integer n2 = n1;
+        System.out.println(n2);
+    }
+}
